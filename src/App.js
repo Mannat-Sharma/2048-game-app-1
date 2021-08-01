b16e5b2fdb2605268824d729d95b85029e31dc72
snverma1409 <snverma1409@gmail.com>
2021-08-02 00:43:13 +0530

    css files updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import MainGame from './components/MainGame';
+import { useState } from "react";
+import "./App.css";
+import MainGame from "./components/MainGame";
+import Motion from "./components/Motion";
+import Over from "./components/Over";
 
 function App() {
+  const [display, setDisplay] = useState(true);
   return (
     <div className="App">
-      <h1>2048</h1>
-      <h2>GAME</h2>
-      <MainGame />
-
+      {display ? (
+        <Motion setDisplay={setDisplay} />
+      ) : (
+        <MainGame setDisplay={setDisplay} />
+      )}
     </div>
   );
 }
